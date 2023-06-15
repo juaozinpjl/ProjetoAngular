9523712bc67f40229a4e70c5d6b32df9f4bec073
juaozinpjl <125033520+juaozinpjl@users.noreply.github.com>
2023-06-15 12:38:34 +0000

    calculattorr

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+
+	exibir(numero: number) {
+		console.log(numero);
+	}
+
 }
