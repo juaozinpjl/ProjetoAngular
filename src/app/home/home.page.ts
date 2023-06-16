5f31cf3120be302de4d5a4081c4ec068b8029556
juaozinpjl <125033520+juaozinpjl@users.noreply.github.com>
2023-06-16 16:05:47 +0000

    projeto da calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,9 +10,60 @@ import { CommonModule } from '@angular/common';
   imports: [IonicModule, CommonModule],
 })
 export class HomePage {
+  //Valores//
+  inputValue : string = '';
+  valor1: any = null;
+  valor2: any = null;
+  resultado: any = null;
+  operador: any = null;
 
-	exibir(numero: number) {
-		console.log(numero);
-	}
+  exibir(numero: any) {
+    if (this.valor1 == null) {
+      this.valor1 = numero
+      this.resultado = numero
+    } else if (this.valor2 == null) {
+      this.valor2 = numero
+      this.resultado = numero
+    }
 
+    console.log(this.valor1);
+    console.log(this.valor2);
+    
+  }
+
+  operacao(operador: any){
+    this.operador=operador
+  }
+  
+  calcular(){
+    if (this.operador == '+') {
+      this.resultado = this.valor1 + this.valor2;
+    } else if (this.operador == '-') {
+      this.resultado = this.valor1 - this.valor2;
+    } else if (this.operador == '*') {
+      this.resultado = this.valor1 * this.valor2;
+    } else if (this.operador == '/') {
+      this.resultado = this.valor1 / this.valor2;
+    }
+    console.log(this.operador);
+  }
+
+  clear() {
+    this.valor1=null
+    this.valor2=null
+    this.resultado=null
+  
+  }
+
+  apagar() {
+  if(this.valor1 != null && this.valor2==null){
+    this.valor1=null
+    this.resultado=null
+  }
+  if(this.valor1 != null && this.valor2==null){
+    this.valor2=null
+    this.resultado=null
+  }
+
+  }
 }
